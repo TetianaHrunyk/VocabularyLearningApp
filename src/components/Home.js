51b7169bf6aeeb8ba6dd41d85dc3610543bd4bf3
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-03-28 11:19:14 +0300

    add forms and some components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
+import Form from "react-bootstrap/Form"
+import {Col} from "react-bootstrap"
+
 const  Home = () => {
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <Form>
+                <Form.Row>
+                    <Col>
+                        <Form.Control placeholder="Input text" />
+                    </Col>
+                    <Col>
+                        <Form.Control placeholder="Translation" />
+                    </Col>
+                </Form.Row>
+            </Form>
         </div> 
     );
 }
