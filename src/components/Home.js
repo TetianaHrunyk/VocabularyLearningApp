14a3ab572b5100f77ce69847b3e501579ceb0301
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-03 08:30:28 +0300

    add parsing to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,29 @@
+import {useState} from "react"
 import Form from "react-bootstrap/Form"
-import {Col, Row, Container, Button} from "react-bootstrap"
+import {Col, Row, Container, Button, Card} from "react-bootstrap"
+import CustomForm from "./CustomForm"
+import CustomDeck from "./CustomDeck"
 
 const  Home = () => {
+    const [parsed, setParse] = useState(false);
+    const [text, setText] = useState("");
+
+    
+    const handleParse = ( (e) => {
+        setParse(true)
+    })
+
+    const handleText = ( (e) => {
+        setText(e.target.value)
+    })
+
     return (
         <div className="home">
             <h2>Homepage</h2><br />
-            <Container>
-                <Row>
-                    <Col>
-                        <h3>Input Text</h3>
-                    </Col>
-                    <Col>
-                        <h3>Translation</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Control as="textarea" placeholder="Input text" />
-                        <Button type="submit">Translate</Button>
-                    </Col>
-                    <Col as="article">
-                        <p>Translation</p>
-                    </Col>
-                </Row>
-            </Container>
+            {!parsed ? 
+                    <CustomForm text = {text} parsed={parsed} handleParse={handleParse} handleText={handleText}/> : 
+                    <CustomDeck text={text} />
+            }            
         </div> 
     );
 }
