48f5de84127488c4aaf12f3b7802697199ac363d
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-05 18:16:46 +0300

    decks, cards pages proto of inline-editable card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,84 @@
+import useFetch from '../hooks/useFetch'
+import {useState} from "react"
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+
 const Cards = () => {
+  const { data, isPending, error } = useFetch('./sampleCards.json');
+  const userDecks = [{"id": 124, "name": "Deck2" }, {"id": 123, "name": "Deck1" }]
+
+  const [deck, setDeck] = useState("124");
+  const [sort, setSort] = useState("added");
+
+  const handleEdit = (e) => {
+    console.log("edit")
+  }
+
+  const handleDelete = (e) => {
+    console.log("delete")
+  }
+
+  const handleSort = (e => {
+    setSort(e.target.value)
+  })
+
+  const handleDeck = (e => {
+    setDeck(e.target.value)
+  })
+
+  function makeDeck(data, deck, sort) {
+    return data.filter(card => card.deckId == deck)
+      .sort(card => card.sort)
+      .map( card => {
+      return (
+        <Card style={{minWidth: "30%"}} key={card.id}>
+          <Card.Body>
+            <Card.Title >
+              {card.deckName}
+            </Card.Title>
+            <Card.Text>
+              Front: {card.front}<br />
+              Back: {card.back}<br />
+              Context: {card.context}<br />
+              Added: {card.added}<br />
+              Revised: {card.revised}<br />
+            </Card.Text>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
+          </Card.Body>
+        </Card>
+      )
+    })
+  }
+
+  function makeSelectDeck(decks) {
+    return decks.map( deck => {
+      return <option value={deck.id} key={deck.id}>{deck.name}</option>
+    })
+  }
+
   return ( 
-    <div>
-      <h2>Cards</h2>
+    <div className="home">
+      <h2>Cards in {deck} </h2>
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { data && 
+        <div>
+
+            <select name="sort" onChange={handleSort}>
+              <option value="added">Added</option>
+              <option value="revised">Revised</option>
+              <option name="front">Front</option>
+              <option name="back">Back</option>
+            </select>
+
+            <select name="sort" onChange={handleDeck}>
+              {makeSelectDeck(userDecks)}
+            </select>
+
+          <CardDeck>{makeDeck(data, deck, sort)}</CardDeck>  
+        </div>
+      }
     </div>
   );
 }
--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import useFetch from '../hooks/useFetch'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
 
 const Decks = () => {
   const { data, isPending, error } = useFetch('./sampleDecks.json');
+  const [sort, setSort] = useState("added");
+
+  const handleEdit = (e) => {
+    console.log("edit")
+  }
 
   const handleDelete = (e) => {
     console.log("delete")
   }
+
+  const handleSort = (e => {
+    setSort(e.target.value)
+    console.log(sort)
+  })
 
   function makeDeck(data) {
     return data.map( deck => {
@@ -20,6 +31,7 @@ const Decks = () => {
             <Card.Text>
               Created: {deck.created}
             </Card.Text>
+            <button onClick={handleEdit}>Edit</button>
             <button onClick={handleDelete}>Delete</button>
           </Card.Body>
         </Card>
@@ -32,7 +44,17 @@ const Decks = () => {
       <h2>Decks</h2>
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { data && <CardDeck>{makeDeck(data)}</CardDeck>  }
+      { data && 
+      <div>
+        <select name="sort" onChange={handleSort}>
+              <option value="added">Added</option>
+              <option value="revised">Revised</option>
+              <option name="front">Front</option>
+              <option name="back">Back</option>
+            </select>
+        <CardDeck>{makeDeck(data)}</CardDeck>  
+      </div>
+      }
     </div>
   );
 }
