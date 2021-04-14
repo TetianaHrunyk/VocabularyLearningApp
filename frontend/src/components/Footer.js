3b8db6f7cbb2f1a72d67376a51b186a6a5fec9e7
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-14 22:32:09 +0300

    delete everything

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <div className="footer">
-            <h4>Footer</h4>
-        </div>
-    );
-}
- 
-export default Footer;
--- frontend/src/components/Study.js
+++
@@ -1,9 +0,0 @@
-const Study = () => {
-  return ( 
-    <div>
-      <h2>Study</h2>
-    </div>
-  );
-}
- 
-export default Study;
