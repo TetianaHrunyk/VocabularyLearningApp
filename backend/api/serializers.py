e1064711975a5a63565327a9102571229bf3b1f4
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-14 22:39:09 +0300

    add backend and docs

---
+++ backend/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Users, Decks, Cards
+
+class UsersSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Users
+    fields = ('userId', 'username', 'email', 'password', 'registered')
+
+class DecksSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Decks
+    fields = ('deckId', 'userId', 'deckName', 'created')
+
+class CardsSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Cards
+    fields = ('id', 'deckId', 'userId', 'front', 'back',
+              'context', 'added', 'revised', 'nat_for', 'for_nat')
