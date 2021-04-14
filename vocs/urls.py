c37dec5f779aca6910c6f6093ff6a53dbfa245ab
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-14 20:11:03 +0300

    tried integrating python, failed

--- vocs/urls.py
+++ vocs/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico"))
+    ),
 ]
