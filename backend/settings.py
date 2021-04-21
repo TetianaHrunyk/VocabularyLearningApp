46f07e442ad97e20ef29082ece92d02661956d00
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-21 10:15:11 +0300

    m

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    '*'
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 REST_FRAMEWORK = {
