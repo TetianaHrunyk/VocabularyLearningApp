b7691d9b9e830a224f9471c2a26f12a5133d3d70
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-21 09:56:26 +0300

    edit django host

--- src/components/paths.js
+++ src/components/paths.js
@@ -1,4 +1,5 @@
 //const django_host =   "http://localhost:8000/"
-const django_host =   "http://vocsapp.herokuapp.com/"
+//const django_host =   "http://vocsapp.herokuapp.com/"
+const django_host =   "/"
 
 export default django_host
