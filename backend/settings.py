4755d95935a60917db90d8a99490d502782ac87a
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-04-21 10:11:05 +0300

    cors whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -141,8 +141,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://localhost:8000',
+    '*'
 ]
 
 REST_FRAMEWORK = {
