7da08722372a37c7176a0680786070653e90a40c
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-05-05 12:00:29 +0300

    make parsed text combinable, no styling

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,29 +1,68 @@
+import { useState, useEffect } from "react"
 import Card from "react-bootstrap/Card"
 import Form from "react-bootstrap/Form"
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Droppable, Draggable, DragDropContext} from "react-beautiful-dnd"
+
 //import UserContext from "../contexts/UserContext"
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
 
 
 const Left = (props) => {
  // const { text, parsed, handleParse, handleText , queue, handleWordClick, back, handleBack} = props
  const { text, parsed, handleParse, handleText , handleWordClick, handleBack} = props
+  const [words, setWords] = useState([]);
+         
+  useEffect(() => {
+    var counter = 0
+    setWords(text.split(" ").filter( elem => elem !== " ")
+                  .map(elem => elem.toLowerCase().replace(/[^\w\s]|_/g, ""))
+                  .map(elem => {counter++; return {val: elem, id: counter}})
+            )
+  }, [text]);
 
-  function parseText(text) {
-    return (
-      text.split(" ")
-      .filter( elem => elem !== " ")
-      .map(elem => elem.toLowerCase().replace(/[^\w\s]|_/g, ""))
-      .map(elem => {
-        return (
-            <button value = {elem} 
-              onClick={handleWordClick} 
-              key={elem+Math.floor(Math.random() * 1000)}
-            >
-              {elem}
-            </button>
-        )
-      })
-    )
+  function onDragStart() {
+    if (window.navigator.vibrate) {
+      window.navigator.vibrate(100);
+    }
+  }
+
+  function onDragEnd(result) {
+    
+    if (result.combine) {
+      const merged = result.combine.draggableId+" "+result.draggableId
+
+      setWords(words
+        .filter(elem => elem.id !== result.source.index)
+        .map(elem => {
+          if (elem.val+elem.id === result.combine.draggableId) {
+            return {val: merged.replace(/[\d]|_/g, ""), id: elem.id}
+          } else { 
+            return elem 
+          }
+        }))
+    }
+
   }
   
   if (!parsed) {
@@ -48,17 +87,50 @@ const Left = (props) => {
     return (
       <Card>
         <Card.Body>
-          <Card.Title>
-            Input Text
-          </Card.Title>
-            {parseText(text)}
-          <Card.Text>
-            <br />
-          </Card.Text>
+          <Card.Title>Input Text</Card.Title>
+            <DragDropContext
+              onDragStart={onDragStart}
+              onDragEnd={onDragEnd}
+              isCombineEnabled={true}
+            >
+              <Droppable droppableId="droppable" isCombineEnabled>
+                {(provided, snapshot) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}
+                  >
+
+                    {words.map(elem => {
+                      return (
+                        <Draggable key={elem.id} draggableId={elem.val+elem.id} index={elem.id} >
+                          {(provided, snapshot) => (
+                            <div 
+                              ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={getItemStyle(
+                                        snapshot.isDragging,
+                                        provided.draggableProps.style
+                                      )}
+                            >
+                              {elem.val}
+                              <br />
+                              <button value={elem.val} onClick={handleWordClick} >Translate</button>
+                            </div>
+                          )} 
+                        </Draggable>
+                      )}
+                    )}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           <button onClick={handleBack}>Back</button>
         </Card.Body>
       </Card>
-    )
+    );
   }
 }
  
